f6f4bff74da03e5b2de70160881d5507305332fa
ViniHGV <ixxvinicius@gmail.com>
2024-09-04 19:44:02 -0300

    feat: criando router e adicionando o router na main.tsx

---
+++ app/src/Router.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CreateCustomers } from "./pages/CreateCustomer";
+import { ListCustomers } from "./pages/ListCustomers";
+import { EditCustomers } from "./pages/EditCustomers";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListCustomers />} />
+        <Route path="/customer/create" element={<CreateCustomers />} />
+        <Route path="/customer/edit/:idUser" element={<EditCustomers />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
