e19177e3346fa28879ff67918d8436fb79a8e0c8
ViniHGV <ixxvinicius@gmail.com>
2024-09-04 19:49:54 -0300

    feat: criando regex para atribuição de máscara no telefone

---
+++ app/src/utils/Masks/PhoneMask.ts
@@ -0,0 +1,7 @@
+export const PhoneMask = (phone: string) => {
+  return phone
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "($1) $2")
+    .replace(/(\d{5})(\d)/, "$1-$2")
+    .replace(/(-\d{4})\d+?$/, "$1");
+};
