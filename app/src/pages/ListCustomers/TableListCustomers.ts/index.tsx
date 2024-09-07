ed2fe32c288297627e54875822226be1761100ca
ViniHGV <ixxvinicius@gmail.com>
2024-09-06 22:46:56 -0300

    feat: consumindo serviço de listagem e todos os clientes, realizando a listagem e componentizando dialogs de detalhes do cliente e deleção do cliente

--- app/src/pages/ListCustomers/TableListCustomers.ts/index.tsx
+++ app/src/pages/ListCustomers/TableListCustomers.ts/index.tsx
@@ -1,4 +1,3 @@
-import { GenericDialog } from "@/components/GenericDialog";
 import { Button } from "@/components/ui/button";
 import {
   Table,
@@ -9,53 +8,32 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { IFormRegisterCustomer } from "@/interfaces/IFormRegisterCustomer";
-import {
-  AlertDialogCancel,
-  AlertDialogAction,
-} from "@radix-ui/react-alert-dialog";
-import { MdOutlineEdit, MdDeleteOutline } from "react-icons/md";
+import { GetAllCustmersService } from "@/services/Customer/GetAllCustmers.Service";
+import { useQuery } from "@tanstack/react-query";
+import { MdOutlineEdit } from "react-icons/md";
 import { useNavigate } from "react-router";
-
-const users: IFormRegisterCustomer[] = [
-  {
-    nome: "Ana Silva",
-    email: "ana.silva@email.com",
-    telefone: "(11) 99999-1234",
-    endereco: "Rua das Flores, 123, São Paulo, SP",
-  },
-  {
-    nome: "Carlos Pereira",
-    email: "carlos.pereira@email.com",
-    telefone: "(21) 98888-5678",
-    endereco: "Avenida Brasil, 456, Rio de Janeiro, RJ",
-  },
-  {
-    nome: "Maria Souza",
-    email: "maria.souza@email.com",
-    telefone: "(31) 97777-9101",
-    endereco: "Rua das Palmeiras, 789, Belo Horizonte, MG",
-  },
-  {
-    nome: "João Oliveira",
-    email: "joao.oliveira@email.com",
-    telefone: "(41) 96666-1213",
-    endereco: "Rua das Acácias, 101, Curitiba, PR",
-  },
-  {
-    nome: "Fernanda Costa",
-    email: "fernanda.costa@email.com",
-    telefone: "(51) 95555-1415",
-    endereco: "Avenida Farrapos, 202, Porto Alegre, RS",
-  },
-];
+import { ImSpinner8 } from "react-icons/im";
+import { DialogDetailsCustomer } from "./DialogDetailsCustomer";
+import { DialogDeleteCustomer } from "./DialogDeleteCustomer";
 
 export function TableListCustomers() {
   const navigate = useNavigate();
 
+  const { data: customers, isLoading } = useQuery({
+    queryKey: ["customers"],
+    queryFn: () => GetAllCustmersService(),
+  });
+
   const navigateToEditCustomer = (idUser: number) => {
     navigate(`customer/edit/${idUser}`);
   };
+
+  if (isLoading)
+    return (
+      <div className="w-screen h-screen flex items-center justify-center">
+        <ImSpinner8 className="animate-spin text-2xl" />
+      </div>
+    );
 
   return (
     <Table>
@@ -70,48 +48,36 @@ export function TableListCustomers() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {users.map((user, index) => (
-          <TableRow key={user.email}>
-            <TableCell className="font-medium py-4">{user.nome}</TableCell>
-            <TableCell>{user.email}</TableCell>
-            <TableCell>{user.telefone}</TableCell>
-            <TableCell>{user.endereco}</TableCell>
-            <TableCell className="text-right">
-              <div className="space-x-2">
-                <Button
-                  onClick={() => navigateToEditCustomer(index)}
-                  className="bg-yellow-500 hover:bg-yellow-600"
-                >
-                  <MdOutlineEdit className="text-lg mr-1" />
-                  Editar
-                </Button>
-                <GenericDialog
-                  buttonOpenDiolog={
-                    <Button variant="destructive">
-                      <MdDeleteOutline className="text-lg mr-1" />
-                      Excluir
-                    </Button>
-                  }
-                  title="Certeza que deseja excluir o cliente?"
-                  description="Excluir um cliente é uma ação irreversivel confirme para continuar"
-                  ButtonsFooter={
-                    <>
-                      <AlertDialogCancel>
-                        <Button variant="outline">Cancelar</Button>
-                      </AlertDialogCancel>
-                      <AlertDialogAction className="bg-red">
-                        <Button variant="destructive">
-                          <MdDeleteOutline className="text-lg mr-1" />
-                          Confirmar exclusão
-                        </Button>
-                      </AlertDialogAction>
-                    </>
-                  }
-                />
-              </div>
-            </TableCell>
-          </TableRow>
-        ))}
+        {(customers?.length as number) > 0 ? (
+          customers?.map((customer) => (
+            <TableRow key={customer.id}>
+              <TableCell className="font-medium py-4">
+                {customer.nome}
+              </TableCell>
+              <TableCell>{customer.email}</TableCell>
+              <TableCell>{customer.telefone}</TableCell>
+              <TableCell>{customer.endereco}</TableCell>
+              <TableCell className="text-right">
+                <div className="space-x-2">
+                  <DialogDetailsCustomer customer={customer} />
+                  <Button
+                    onClick={() => navigateToEditCustomer(customer.id)}
+                    className="bg-yellow-500 hover:bg-yellow-600"
+                  >
+                    <MdOutlineEdit className="text-lg mr-1" />
+                    Editar
+                  </Button>
+                  <DialogDeleteCustomer
+                    customerName={customer.nome}
+                    customerId={customer.id}
+                  />
+                </div>
+              </TableCell>
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>Sem clientes cadastrados</TableRow>
+        )}
       </TableBody>
     </Table>
   );
