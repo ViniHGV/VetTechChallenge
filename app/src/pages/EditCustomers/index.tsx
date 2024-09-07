374d1907c6ad06b0fe308159c949645eed7fa0f4
ViniHGV <ixxvinicius@gmail.com>
2024-09-06 22:42:21 -0300

    feat: consumindo serviço para a busca de um unico cliente e passando por parêmetro o customer para o form de edição

--- app/src/pages/EditCustomers/index.tsx
+++ app/src/pages/EditCustomers/index.tsx
@@ -1,16 +1,33 @@
 import { useParams } from "react-router";
 import logoVetTech from "/public/LogoVet.png";
 import { GenericFormCustomer } from "@/components/GenericFormCustomer";
+import { useQuery } from "@tanstack/react-query";
+import { GetUniqueCustomerByIdService } from "@/services/Customer/GetUniqueCustomerById.Service";
 
 export const EditCustomers = () => {
-  const { idUser } = useParams();
+  const { customerId } = useParams();
+
+  const { data: customer } = useQuery({
+    queryKey: ["customer"],
+    queryFn: () => {
+      if (customerId) return GetUniqueCustomerByIdService(customerId);
+    },
+  });
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen ">
       <div className="flex flex-col w-[450px] space-y-6  items-center p-10 rounded-xl shadow-2xl">
         <img src={logoVetTech} alt="Logo VetTech" />
         <GenericFormCustomer
-          customerData={{ email: "", endereco: "", nome: "", telefone: "" }}
+          customerData={
+            customer && {
+              id: customer?.id,
+              email: customer?.email,
+              endereco: customer?.endereco,
+              nome: customer?.nome,
+              telefone: customer?.telefone,
+            }
+          }
         />
       </div>
     </div>
