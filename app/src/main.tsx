97c81a2f2e6ef9dec65dd0e38443bdcdf2c4c4b7
ViniHGV <ixxvinicius@gmail.com>
2024-09-06 22:43:26 -0300

    feat: criando instância de query client e adicionando provider do query client junto com toastcontainer

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Router } from "./Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+      <ToastContainer />
+    </QueryClientProvider>
   </StrictMode>
 );
