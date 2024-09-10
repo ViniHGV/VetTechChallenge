44c7d96d6e950d176dbe4aefa6d3292f2ef6aaaa
ViniHGV <ixxvinicius@gmail.com>
2024-09-09 22:25:49 -0300

    feat: retirando obrigatóriedade de campos opcionais

--- app/src/schemas/FormRegisterCustomerSchema.ts
+++ app/src/schemas/FormRegisterCustomerSchema.ts
@@ -5,6 +5,6 @@ export const FormRegisterCustomerSchema: ZodType<IFormRegisterCustomer> =
   z.object({
     nome: z.string().min(1, { message: "O campo nome é obrigatório!" }),
     email: z.string().min(1, { message: "O campo e-mail é obrigatório!" }),
-    telefone: z.string().min(1, { message: "O campo telefone é obrigatório!" }),
-    endereco: z.string().min(1, { message: "O campo endereço é obrigatório!" }),
+    telefone: z.string(),
+    endereco: z.string(),
   });
